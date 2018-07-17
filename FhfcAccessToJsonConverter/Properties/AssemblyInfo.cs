b73745098c65a96557070cf0595887a40bab9fb6
dkearsley89 <34358556+dkearsley89@users.noreply.github.com>
2018-07-17 22:32:30 +0930

    Add .NET Project

---
+++ FhfcAccessToJsonConverter/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FhfcAccessToJsonConverter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FhfcAccessToJsonConverter")]
+[assembly: AssemblyCopyright("Copyright © David Kearsley 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("771797f1-2d97-4218-8352-2679075211bd")]
+// Version information for an assembly consists of the following four values:
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.*")]
